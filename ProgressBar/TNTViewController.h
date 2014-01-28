f2364727893abdd85914769eb801664905f71690
Joe Selvik <selvi041@umn.edu>
2014-01-28 10:44:47 -0600

    Initial Commit

---
+++ ProgressBar/TNTViewController.h
@@ -0,0 +1,13 @@
+//
+//  TNTViewController.h
+//  ProgressBar
+//
+//  Created by Joe Selvik on 1/28/14.
+//  Copyright (c) 2014 Joe Selvik. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TNTViewController : UIViewController
+
+@end
